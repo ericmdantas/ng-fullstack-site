6190c172ce281494aa470231e2968d9879b2a200
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-08-03 08:13:57 -0300

    .

--- client/dev/terminal/index.js
+++ client/dev/terminal/index.js
@@ -9,38 +9,63 @@ module.exports = {
     return {
         interactions: [
           {
-            question: 'Something?',
-            answer: 'yes',
+            question: 'What is the name of your app?',
+            answer: '',
             answered: false
           },
           {
-            question: 'Something?',
-            answer: 'yes',
+            question: 'What is your username?',
+            answer: '',
             answered: false
           },
           {
-            question: 'Something?',
-            answer: 'yes',
+            question: 'What is your email?',
+            answer: '',
             answered: false
           },
           {
-            question: 'Something?',
-            answer: 'yes',
+            question: 'What stack do you want?',
+            answer: '',
             answered: false
           },
           {
-            question: 'Something?',
-            answer: 'yes',
+            question: 'Which Git repository hosting service are you using?',
+            answer: '',
             answered: false
           },
           {
-            question: 'Something?',
-            answer: 'yes',
+            question: 'Do you want to keep test in a separated directory?',
+            answer: '',
             answered: false
           },
           {
-            question: 'Something?',
-            answer: 'yes',
+            question: 'What do you want in server side?',
+            answer: '',
+            answered: false
+          },
+          {
+            question: 'What do you want in client side?',
+            answer: '',
+            answered: false
+          },
+          {
+            question: 'What framework do you want to use in server side?',
+            answer: '',
+            answered: false
+          },
+          {
+            question: 'What transpiler do you want to use in server side?',
+            answer: '',
+            answered: false
+          },
+          {
+            question: 'Do you want a secure app (HTTP/2)?',
+            answer: '',
+            answered: false
+          },
+          {
+            question: 'Do you want to use a different static server? Such as nginx, apache, IIS?',
+            answer: '',
             answered: false
           }
         ]
