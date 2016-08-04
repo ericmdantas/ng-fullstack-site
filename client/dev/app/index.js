060b1beb02e6abf776a2ffa4c1bf0848e7c2d83e
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-08-03 22:42:34 -0300

    .

--- client/dev/app/index.js
+++ client/dev/app/index.js
@@ -1,6 +1,18 @@
 import './app.css';
 
 module.exports = {
+  data() {
+    return {
+      css: {
+        height: '0px'
+      }
+    }
+  },
+  ready() {
+    this.css = {
+      height: `${document.body.offsetHeight}px`
+    }
+  },
   template: require('./app.html'),
   components: {
     'n-header': require('../header/index.js'),
