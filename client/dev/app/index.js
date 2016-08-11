343394323298f550ae20f1fa86bcbda2598c5259
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-08-10 21:47:49 -0300

    .

--- client/dev/app/index.js
+++ client/dev/app/index.js
@@ -1,10 +1,11 @@
 import './app.css';
 
-module.exports = {  
+module.exports = {
   template: require('./app.html'),
   components: {
     'n-header': require('../header/index.js'),
     'n-face': require('../face/index.js'),
+    'n-install': require('../install/index.js'),
     'n-stack': require('../stack/index.js'),
     'n-simulation': require('../simulation/index.js'),
     'n-footer': require('../footer/index.js')
---
+++ client/dev/install/index.js
@@ -0,0 +1,5 @@
+import './install.css';
+
+module.exports = {
+  template: require('./install.html')
+}
