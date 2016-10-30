345fe877791aee14b44b42a0adbbd53805d30470
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-10-30 20:37:24 -0200

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,46 @@
-const webpack = require('webpack');
+const path = require('path');
 
-const {
-  join,
-  resolve
-} = require('path');
-
-module.exports = (env) => {
-  return {
-    devtool: 'source-map',
-    entry: './client/dev/index.js',
-    output: {
-      path: './client/dev/',
-      publicPath: '/client/dev',
-      filename: 'bundle.js'
-    },
-    resolve: {
-      root: resolve(__dirname, '.')
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.css$/,
-          loader: 'style!css!'
-        },
-        {
-          test: /\.js$/,
-          loader: 'babel',
-          exclude: /(node_modules)/
-        },
-        {
-          test: /\.html$/,
-          loader: 'html'
-        },
-        {
-          test: /\.png$/,
-          lodader: 'file'
-        },
-        {
-          test: /\.(jpe?g|png|gif|svg)$/i,
-          loaders: [
-              'file?hash=sha512&digest=hex&name=[hash].[ext]',
-              'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-          ]
-        }
-      ]
+module.exports = {
+  devtool: 'source-map',
+  entry: './client/dev/index.js',
+  output: {
+    path: './client/dev/',
+    publicPath: '/client/dev',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    root: path.resolve(__dirname, '.'),
+    alias: {
+      vue: 'vue/dist/vue.js',
+      ubus: 'ubus/dist/commonjs/bus.min.js'
     }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style!css!'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /(node_modules)/
+      },
+      {
+        test: /\.html$/,
+        loader: 'html'
+      },
+      {
+        test: /\.png$/,
+        lodader: 'file'
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      }
+    ]
   }
 }
